7197291e73d75d20b1fa073b609d22167fd22ce9
tommanni <108432153+tommanni@users.noreply.github.com>
2022-10-14 15:40:32 +0300

    Delete src directory

--- src/Nappain.js
+++
@@ -1,9 +0,0 @@
-function Nappain(props) {
-
-    return (
-
-        <button onClick={() => props.handleClick(props.nappain)} className={props.tyyppi}>{props.nappain}</button>
-    );
-}
-
-export default Nappain;
